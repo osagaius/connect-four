99fe2f57173f3bd73945308972d2f55bc39e3990
Osayame Gaius-Obaseki <osayame@gmail.com>
2017-04-11 22:59:57 -0400

    add uuid dep

--- mix.exs
+++ mix.exs
@@ -17,7 +17,7 @@ defmodule ConnectFour.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {ConnectFour, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext]]
+     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext, :uuid]]
   end
 
   # Specifies which paths to compile per environment.
@@ -28,11 +28,14 @@ defmodule ConnectFour.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:uuid, "~> 1.1"}
+   ]
   end
 end
