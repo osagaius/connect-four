3f7eaccfc449c80fb4bdff31e00009a2f190d573
Osayame Gaius-Obaseki <osayame@gmail.com>
2017-04-30 17:51:17 -0400

    add api route to create new game

--- web/router.ex
+++ web/router.ex
@@ -13,10 +13,16 @@ defmodule ConnectFour.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/api" do
+    pipe_through :api
+
+    post "/game", ConnectFour.GameController, :create
+  end
+
   scope "/", ConnectFour do
     pipe_through :browser # Use the default browser stack
 
-    get "/", PageController, :index
+    get "*path", PageController, :index
   end
 
   # Other scopes may use custom stacks.
