e11ba4061315154db6680a764b46673320c86294
Osayame Gaius-Obaseki <osayame@gmail.com>
2017-05-05 19:29:54 -0500

    refactor

--- web/controllers/game_controller.ex
+++ web/controllers/game_controller.ex
@@ -23,16 +23,18 @@ defmodule ConnectFour.GameController do
     {col, _} = params["column"] |> Integer.parse
     game_id = params["game_id"]
 
-    pid = Game.get_game_process_name(game_id)
-    |> Process.whereis
+    pid = Game.get_game_process_name(game_id) |> Process.whereis
 
-    Game.drop_disc(pid, col)
+    case resp = Game.drop_disc(pid, col) do
+      {:error, msg} ->
+        send_resp(conn, 400, msg)
+      {:ok, game} ->
+        Logger.warn("drop disc resp #{inspect game}")
+        game = game
+        |> convert_board
 
-    :timer.sleep(250)
-
-    game = get_game(pid)
-
-    send_resp(conn, 200, game |> Poison.encode!)
+        send_resp(conn, 200, game |> Poison.encode!)
+    end
   end
 
   defp convert_keys(map) do
@@ -42,7 +44,10 @@ defmodule ConnectFour.GameController do
 
   defp get_game(pid) do
     game = Game.get_game_status(pid)
+    |> convert_board
+  end
 
+  defp convert_board(game) do
     board = game.board
     |> Map.new(fn {k, v} -> {Integer.to_string(k), v |> convert_keys} end)
     |> Matrix.to_list
